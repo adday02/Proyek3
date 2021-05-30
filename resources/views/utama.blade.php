b9df2fa0c365c0be4e551fec0f98ae83675c2f48
lailatululwiyah22 <lailatululwiyah22@gmail.com>
2021-05-30 20:25:28 +0700

    revisi lagi day footernya

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -53,7 +53,7 @@
           <li><a href="#about">Fitur</a></li>
           <li><a href="#services">Pelatihan</a></li>
           <li><a href="#portfolio">Perusahaan</a></li>
-          <li><a href="#team">Pegawai</a></li>
+         
           <li><a href="#contact">Kontak</a></li>
 
           <li class="get-started"><a href="{{URL('/daftar')}}">Daftar</a></li>
@@ -181,85 +181,11 @@
       </div>
     </section><!-- End Portfolio Section -->
     <!-- ======= Team Section ======= -->
-    <section id="team" class="team">
-      <div class="container">
+   
 
-        <div class="section-title" data-aos="fade-up">
-          <p>Pegawai Disnaker</p>
-        </div>
+         
 
-        <div class="row">
-
-          <div class="col-xl-3 col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="100">
-            <div class="member">
-              <img src="assets/img/team/team-1.jpg" class="img-fluid" alt="">
-              <div class="member-info">
-                <div class="member-info-content">
-                  <h4>Walter White</h4>
-                  <span>Chief Executive Officer</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
-            <div class="member">
-              <img src="assets/img/team/team-2.jpg" class="img-fluid" alt="">
-              <div class="member-info">
-                <div class="member-info-content">
-                  <h4>Sarah Jhonson</h4>
-                  <span>Product Manager</span>
-                </div>
-                <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="300">
-            <div class="member">
-              <img src="assets/img/team/team-3.jpg" class="img-fluid" alt="">
-              <div class="member-info">
-                <div class="member-info-content">
-                  <h4>William Anderson</h4>
-                  <span>CTO</span>
-                </div>
-                <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-xl-3 col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="400">
-            <div class="member">
-              <img src="assets/img/team/team-4.jpg" class="img-fluid" alt="">
-              <div class="member-info">
-                <div class="member-info-content">
-                  <h4>Amanda Jepson</h4>
-                  <span>Accountant</span>
-                </div>
-                <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Team Section -->
+         
     <!-- ======= Contact Us Section ======= -->
     <section id="contact" class="contact">
       <div class="container">
@@ -338,58 +264,18 @@
   <!-- ======= Footer ======= -->
   <footer id="footer">
 
-    <div class="footer-newsletter" data-aos="fade-up">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6">
-            <h4>Join Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+    
 
     <div class="footer-top">
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-contact" data-aos="fade-up" data-aos-delay="100">
-            <h3>Ninestars</h3>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-          </div>
+          
+          
 
-          <div class="col-lg-3 col-md-6 footer-links" data-aos="fade-up" data-aos-delay="200">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
+         
 
-          <div class="col-lg-3 col-md-6 footer-links" data-aos="fade-up" data-aos-delay="300">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links" data-aos="fade-up" data-aos-delay="400">
+          <!--<div class="col-lg-3 col-md-6 footer-links" data-aos="fade-up" data-aos-delay="400">
             <h4>Our Social Networks</h4>
             <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
             <div class="social-links mt-3">
@@ -403,18 +289,18 @@
 
         </div>
       </div>
-    </div>
+    </div>-->
 
     <div class="container py-4">
       <div class="copyright">
-        &copy; Copyright <strong><span>Ninestars</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>DISNAKER INDRAMAYU</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
         <!-- All the links in the footer should remain intact. -->
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+       <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
       </div>
     </div>
   </footer><!-- End Footer -->
---
+++ tests/Feature/edit_lowongantets.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+//use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Lowongan;
+
+class edit_lowongantets extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testedit_lowongan()
+    {
+        
+        //edit data
+
+        $lowongans = Lowongan::find(2);
+
+        $lowongans->id_perusahaan     = '1805034';
+        $lowongans->nama              = 'admin';
+        $lowongans->jenis_kerja       ='Magang';
+        $lowongans->deskripsi_kerja   = 'admin adalah';
+        $lowongans->lokasi_kerja       = 'bandung';
+        $lowongans->gaji                = '120000000';
+        $lowongans->kontak              = '0896754321';
+        $lowongans->status              = 'Diterima';
+        
+        
+        $lowongans->save();
+
+        //cek data apakah ada di db, jika ya  hasilnya OKE
+        $this->assertDatabaseHas('lowongans', [
+
+            'id_perusahaan'=> '1805034',
+            'nama' => 'admin',
+            'jenis_kerja' => 'Magang',
+            'deskripsi_kerja'   => 'admin adalah',
+            'lokasi_kerja'  => 'bandung',
+            'gaji'    => '120000000',
+            'kontak' => '0896754321',
+            'status' => 'Diterima',
+            
+        ]);
+
+
+        //$response = $this->get('/');
+
+        //$response->assertStatus(200);
+    }
+}
